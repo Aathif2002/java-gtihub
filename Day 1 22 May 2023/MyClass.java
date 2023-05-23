097ad6fa49f5670594f3c197b134fd2d1a19fa0b
Aathif2002 <aathifpbt@gmail.com>
2023-05-23 10:49:49 +0530

    first commit

---
+++ Day 1 22 May 2023/MyClass.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class MyClass
+{
+        int a=56;
+        public static void main(String[] args)
+        {
+          MyClass mc= new MyClass();
+          System.out.println("welcome aathif");
+          System.out.println("a ="+ mc.a);
+         }
+}
