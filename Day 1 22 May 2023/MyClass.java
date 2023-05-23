32417c1e374418859a1ea60644e491331d7541ee
Aathif2002 <aathifpbt@gmail.com>
2023-05-23 11:05:32 +0530

    first

--- Day 1 22 May 2023/MyClass.java
+++ Day 1 22 May 2023/MyClass.java
@@ -1,12 +1,37 @@
-import java.lang.*;
+package com.dal;
+	
+	
+ class MyClass {
+	 
+	 int a=9;
+	
+	public void display()
+	{
+		int a=23;
+		int b=56;
+		int c=a+b;
+		System.out.println("a+b ="+c);
+	}
+	
+	public void aathif()
+	{
+		String a="Mohammed"; 
+		String b="Aathif";
+		System.out.println("My Name is :"+a+b);
+	}
+	
 
-class MyClass
-{
-        int a=56;
-        public static void main(String[] args)
-        {
-          MyClass mc= new MyClass();
-          System.out.println("welcome aathif");
-          System.out.println("a ="+ mc.a);
-         }
+
+	public static void main(String[] args) {
+		MyClass nc= new MyClass();
+		nc.display();
+		
+		MyClass mc= new MyClass();
+		mc.aathif();
+		
+		System.out.println(nc.a);
+		
+		
+	}
+
 }
